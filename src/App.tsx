63efc9cc174d806693d297a4093733605625b011
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-12-03 14:32:52 +0100

    no-content message

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import './App.css';
 import { Navbar } from './NavbarComponent/Navbar';
 import { AirportCard } from './AirportCardComponent/AirportCard';
 import { data } from './data/data';
-import { Airport } from './AirportCardComponent/Airport';
+import { IconArchive } from '@tabler/icons'
 
 function App() {
 
   const [airports, setAirports] = useState(data)
 
+  const [showFavourite, setShowFavourite] = useState(false)
+
   const updateAirports = (id: number, like:boolean) => {
-    console.log('update');
     setAirports(airports.map(airport => {
       if(id !== airport.id) return airport
       airport.like = like
       return airport
     }))
+
+    setShowFavourite(airports.filter(airport => airport.like).length !== 0)
   }
 
   return (
@@ -24,7 +27,12 @@ function App() {
       <Navbar />
       <div className='airports-container'>
         <h2>Oblíbená letiště</h2>
-        {airports.filter(airport => airport.like).map(airport => <AirportCard airport={airport} likeHandler={updateAirports} />)}
+        {showFavourite ? airports.filter(airport => airport.like).map(airport => <AirportCard airport={airport} likeHandler={updateAirports} />) :
+         <div className='no-content'>
+           <div>Zatím jste nic nepřidali do oblíbených</div>
+            <IconArchive size={50} />
+         </div>
+         }
       </div>
       <div className='airports-container' >
         <h2>Všechna letiště</h2>
